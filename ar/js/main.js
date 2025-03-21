1cd206d89f4c215cee1bae5be2b29982a74c635d
Ziad.Abuolila <ziad.abuolila@gmail.com>
2025-03-22 00:23:59 +0200

    Add files via upload

---
+++ ar/js/main.js
@@ -0,0 +1,19 @@
+// loader
+window.onload = function(){
+    setTimeout(function () {
+        (function ($) { 
+            $('#loader').removeClass('show');
+            $('.loader').addClass('d-none');
+        })(jQuery);
+    }, 1150)
+}
+// menu
+const btnMenu = document.getElementById('btn-menu');
+const menu = document.getElementById('menu');
+btnMenu.addEventListener('click', function() {
+    if (menu.style.display === 'none' || menu.style.display === '') {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+});
